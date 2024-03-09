08fe64227232bad0bb0619737abe7a21924d0481
MuhammadButt23 <162154274+MuhammadButt23@users.noreply.github.com>
2024-03-09 05:55:06 +0000

    Add files via upload

--- confirmation1.php
+++ confirmation1.php
@@ -21,9 +21,12 @@ include('includes/dbconfig.php');
   <!-- Slick Slider  CSS -->
   <link rel="stylesheet" href="plugins/slick-carousel/slick/slick.css">
   <link rel="stylesheet" href="plugins/slick-carousel/slick/slick-theme.css">
+  <script src="https://kit.fontawesome.com/fbed98dcbf.js" crossorigin="anonymous"></script>
 
   <!-- Main Stylesheet -->
   <link rel="stylesheet" href="css/style.css">
+
+  <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 
 </head>
 
@@ -37,7 +40,7 @@ include('includes/dbconfig.php');
             </button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
+                <ul class="navbar-nav mx-auto">
                     <li class="nav-item">
                         <a class="nav-link" href="index.php">Home</a>
                     </li>
@@ -46,8 +49,6 @@ include('includes/dbconfig.php');
                             Services
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdownServices">
-                            <a class="dropdown-item" href="Loginpage.php">loginpage</a>
-                            <a class="dropdown-item" href="signuppage.php">signuppage</a>
                             <a class="dropdown-item" href="appoinment.php">appoinment</a>
                             <a class="dropdown-item" href="assign_carer.php">assigned carer</a>
 							<a class="dropdown-item" href="confirmation_pstient.php">patient confirmation</a>
@@ -55,23 +56,21 @@ include('includes/dbconfig.php');
 							<a class="dropdown-item" href="confirmation1.php">confirmation</a>
                         </div>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">About Us</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Blog</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Contact</a>
+                        <a class="nav-link" href="blog.php">Blog</a>
                     </li>
                 </ul>
-                <div class="ml-auto">
-                    <a href="appoinment.php" class="btn btn-main mr-3">Schedule a appoinment</a>
+                <div class="ml-lg-4">
+                    <a href="#" class="btn btn-main mr-3">Schedule a Consultation</a>
                     <a href="#" class="btn btn-outline-light">Emergency Contacts</a>
                 </div>
+                 <form method="post">
+                            <button type="submit" name="logout" class="btn btn-outline-dark">Log Out</button>
+                        </form>
             </div>
         </div>
     </nav>
 </header>
+ 
 <section class="section confirmation">
   <div class="container">
     <div class="row justify-content-center">
@@ -88,50 +87,50 @@ include('includes/dbconfig.php');
     </div>
   </div>
 </section>
-
-<!-- footer Start -->
-<footer class="footer section gray-bg">
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-4 mr-auto col-sm-6">
-				<div class="widget mb-5 mb-lg-0">
-					<div class="logo mb-4">
-						<img src="images/Arrow_logo.png" alt="" class="img-fluid">
-					</div>
-
-					<ul class="list-inline footer-socials mt-4">
-						<li class="list-inline-item"><a href="#"><i class="icofont-facebook"></i></a></li>
-						<li class="list-inline-item"><a href="#"><i class="icofont-twitter"></i></a></li>
-					</ul>
-				</div>
-			</div>
-
-			<div class="col-lg-3 col-md-6 col-sm-6">
-				<div class="widget widget-contact mb-5 mb-lg-0">
-					<h4 class="text-capitalize mb-3">Get in Touch</h4>
-					<div class="divider mb-4"></div>
-
-					<div class="footer-contact-block mb-4">
-						<div class="icon d-flex align-items-center">
-							<i class="icofont-email mr-3"></i>
-							<span class="h6 mb-0">Support Available for 24/7</span>
-						</div>
-						<h4 class="mt-2"><a href="tel:+23-345-67890">Support@email.com</a></h4>
-					</div>
-
-					<div class="footer-contact-block">
-						<div class="icon d-flex align-items-center">
-							<i class="icofont-support mr-3"></i>
-							<span class="h6 mb-0">Mon to Fri : 08:30 - 18:00</span>
-						</div>
-						<h4 class="mt-2"><a href="tel:+23-345-67890">+23-456-6588</a></h4>
-					</div>
-				</div>
-			</div>
-		</div>
-		
-	</div>
 </footer>
+<!-- footer Start -->
+<footer>
+    <div class="row">
+        <div class="col">
+                <p>Novena Health & Care Medical Center is renowned for its exceptional healthcare services. With an unwavering dedication to our patients' welfare, we embrace compassion, expertise, and ingenuity in all facets of our care. From state-of-the-art medical procedures to individualized attention, we place your health journey at the forefront. At Novena Health & Care, we go beyond being mere providers; we become allies on your quest for well-being, offering unmatched assistance and direction at every juncture. Count on us to provide top-tier medical care,
+                as your health always comes first in our practice.</p>
+                </div>
+                <div class="col">
+                    <h3>Contact Details</h3>
+                    <p>Support Available for 24/7 </p>
+                    <p>Mon to Fri : 08:30 - 18:00</p> 
+                    <p class="email-id">Support@email.com</p>
+                    <h4>+23-456-6588</h4>
+                 </div>
+                <div class="col">
+                    <h3>Links</h3>
+                    <ul>
+                        <li><a href="">About us</a></li>
+                        <li><a href="">contact form</a></li>
+                        <li><a href="https://www.gov.uk/help/privacy-notice">Privacy Poilices</a></li>
+                        <li><a href="https://www.gov.uk/copyright">Copy Rights</a></li>
+                     </ul>
+                </div>
+                <div class="col">
+                    <h3>News letter</h3>
+                    <form>
+                    <i class="fa-regular fa-envelope"></i>
+                        <input Type="email" placeholder="enter your email id" required>
+                        <button type="submit"><i class="fa-solid fa-arrow-right"></i></button>
+                    </form>
+                    <div class="Social-icons">
+                        <i class="fa-brands fa-linkedin"></i>
+                        <i class="fa-brands fa-twitter"></i>
+                        <i class="fa-brands fa-pinterest"></i>
+                        <i class="fa-brands fa-square-instagram"></i>
+                    </div>
+                        
+                </div>
+        </div>
+        <hr>
+        <p class="copyright">Novena Health & Carers Medical site @ 2024 ~ All Rights Reserved</p>
+</footer>
+
    
 
     <!-- 
